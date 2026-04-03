fc17f1baf846027d92eb0e4483b078597219a9de
praveensuthar108 <praveenjangid107@gmail.com>
2026-04-04 00:36:36 +0530

    contact add mobile no

--- Models/Enquiries.cs
+++ Models/Enquiries.cs
@@ -4,21 +4,21 @@ namespace SRVIndia.Models
 {
     public class Enquiries
     {
-        public int Id { get; set;}
+        public int Id { get; set; }
         [Required]
-        public string PersonName { get; set; }    
+        public string PersonName { get; set; }
 
         public string UserEmail { get; set; }
 
         public string CompanyName { get; set; }
 
-        public string WhatsAppNo { get; set; }
+        public string MobileNo { get; set; }
 
         public string UserMessage { get; set; }
 
         public string BusinessType { get; set; }
 
-        public string UseCargo { get; set; }
+        public string States { get; set; }
 
     }
 }
